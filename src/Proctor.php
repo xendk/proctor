d76e7b7e0f8d85d1f5853a948ef68a0718533151
Thomas Fini Hansen <xen@xen.dk>
2015-02-19 21:02:55 +0100

    Initial commit.

---
+++ src/Proctor.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Proctor;
+
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Logger\ConsoleLogger;
+use Psr\Log\LogLevel;
+use Symfony\Component\Console\Formatter\OutputFormatterStyle;
+use Symfony\Component\Process\ProcessBuilder;
+use Symfony\Component\Process\Process;
+
+class Proctor
+{
+    const VERSION = "0.0.1";
+}
